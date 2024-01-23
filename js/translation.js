63bc54115d638df5f588b5e8d19d2b75dd160d20
Elouan Le Merle <elouan.le-merle@etu.u-bordeaux.fr>
2024-01-23 12:46:44 +0100

    change path

--- js/translation.js
+++ js/translation.js
@@ -5,8 +5,8 @@ let langs = [];
 
 onload = () => {
 	flags.forEach(flag => {
-		// flag.style.backgroundImage = 'url(/PortfolioFlex/ressources/icon/lang/'+flag.classList[0]+'.png)';
-		flag.style.backgroundImage = 'url(/ressources/icon/lang/'+flag.classList[0]+'.png)';
+		flag.style.backgroundImage = 'url(/PortfolioFlex/ressources/icon/lang/'+flag.classList[0]+'.png)';
+		// flag.style.backgroundImage = 'url(/ressources/icon/lang/'+flag.classList[0]+'.png)';
 		langs.push(flag.classList[0]);
 		if(flag.classList[flag.classList.length-1] != 'dropbtn') {
 			flag.addEventListener('click', () => {
@@ -28,8 +28,8 @@ class Translate{
 	process(){
 		let _self = this;
 		//load content data 
-		fetch("/ressources/lang/"+this.lng+".json")
-		// fetch("/PortfolioFlex/ressources/lang/"+this.lng+".json")
+		// fetch("/ressources/lang/"+this.lng+".json")
+		fetch("/PortfolioFlex/ressources/lang/"+this.lng+".json")
 		.then(response => response.json())
 		.then(data => {
 			let allDom = document.getElementsByTagName("*");
